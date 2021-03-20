9fbabce08825417499726910abbabe320bcd1741
danya_by <d.foteeff@yandex.ru>
2021-03-20 22:17:43 +0500

    Количество гласных в строке

---
+++ task_1/vowels.py
@@ -0,0 +1,8 @@
+string = input("Введите строку для анализа: ")
+
+print(f'Количество "a" - {string.count("a")}')
+print(f'Количество "e" - {string.count("e")}')
+print(f'Количество "i" - {string.count("i")}')
+print(f'Количество "o" - {string.count("o")}')
+print(f'Количество "u" - {string.count("u")}')
+print(f'Количество "y" - {string.count("y")}')
